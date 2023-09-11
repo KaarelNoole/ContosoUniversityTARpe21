3a2f07da5053521111a7fd7da530c9f1e583fbf9
KaarelNoole <kaarelnoole@gmail.com>
2023-09-11 16:29:14 +0300

    fix wordline

--- ContosoUniversityTARpe21/Models/Instructor.cs
+++ ContosoUniversityTARpe21/Models/Instructor.cs
@@ -27,6 +27,6 @@ namespace ContosoUniversityTARpe21.Models
         [Display(Name = "Dire Date")]
         public DateTime HireDate { get; set; }
         public ICollection<CourseAssignment> CourseAssignments { get; set; } 
-        public OfficeAssignment OfficeAssignment { get; set; }
+        public OfficeAssignment OfficeAssignments { get; set; }
     }
 }
