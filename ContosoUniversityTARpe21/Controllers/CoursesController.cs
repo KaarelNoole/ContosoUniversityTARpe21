b9cb0ab295a82c210bf1252b8240261e23c9104d
KaarelNoole <kaarelnoole@gmail.com>
2023-10-11 09:12:22 +0300

    update controller

--- ContosoUniversityTARpe21/Controllers/CoursesController.cs
+++ ContosoUniversityTARpe21/Controllers/CoursesController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
-namespace UniversityRemusLuht.Controllers
+namespace ContosoUniversityTARpe21.Controllers
 {
     public class CoursesController : Controller
     {
